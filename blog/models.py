fc580667e67119edf6c98cbb2f78baf337a82aa4
Oliver Otchere <oliverotchere4@gmail.com>
2020-10-03 07:15:03 +0000

    Job app added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class blog(models.Model):
+    Title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    images = models.ImageField(upload_to='images/')
